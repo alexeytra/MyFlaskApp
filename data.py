2d799d067d54ad94afdf525afca17a498e7165c8
Alex <alekceytr@mail.ru>
2018-09-19 10:54:31 +0800

    create flask project and add some pages and logic without database

---
+++ data.py
@@ -0,0 +1,30 @@
+def articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article one',
+            'content': 'blablalbld;lf;ldfdf;dfoikopsjkfkjsd sdjkf;lkjdslfk;sak',
+            'author': 'Mark Kol',
+            'create_date': '04-25-2017',
+        },
+
+        {
+            'id': 2,
+            'title': 'Article TWo',
+            'content': 'blablalbld;lf;ldfdf;dfoikopsjkfkjsd sdjkf;lkjdslfk;sak',
+            'author': 'Helena Dog',
+            'create_date': '04-25-2017',
+        },
+
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'content': 'blablalbld;lf;ldfdf;dfoikopsjkfkjsd sdjkf;lkjdslfk;sak',
+            'author': 'Emma Potter',
+            'create_date': '04-25-2017',
+        }
+
+
+    ]
+
+    return articles
