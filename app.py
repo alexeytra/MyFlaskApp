549462b8620d7eb176d374f33dab9609efb8ceaf
Alex <alekceytr@mail.ru>
2018-09-21 11:47:35 +0800

    add only lib:)

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for, session, logging
 from data import articles
+
+
 
 app = Flask(__name__)
 app.debug = True
